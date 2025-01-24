24cac3da73891467c21edd8bf9d34ede05a3db70
craftomeCJ <javier.ho@activate.sg>
2025-01-25 01:13:59 +0800

    [MOBILE-CODE-TEST][CJ][HEALTH-CONCERNS][Update]: wrap app with recoil provider
    Introduces navigation functionality using React Navigation library
    Includes a WelcomeScreen component with a welcome message and start button

--- App.tsx
+++ App.tsx
@@ -3,21 +3,32 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { RootStackParamList } from './types/navigation';
 import { WelcomeScreen } from './screens/welcome-screen';
+import HealthConcernsScreen from './screens/health-concerns-screen';
+import { RecoilRoot } from 'recoil';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{
-          headerShown: false,
-          cardStyle: { backgroundColor: '#F7FAFC' },
-        }}
-      >
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Welcome'
+          screenOptions={{
+            headerShown: false,
+            cardStyle: { backgroundColor: '#F7FAFC' },
+          }}
+        >
+          <Stack.Screen
+            name='Welcome'
+            component={WelcomeScreen}
+          />
+          <Stack.Screen
+            name='HealthConcerns'
+            component={HealthConcernsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
